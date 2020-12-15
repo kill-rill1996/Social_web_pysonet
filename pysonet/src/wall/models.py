e6ea66b97faeb74b313580c17134bad6e07227c6
Kirill Andreev <Hizenberg228@,ail.ru>
2020-12-15 20:43:37 +0300

    Added Posts and Comments

---
+++ pysonet/src/wall/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+from pysonet import settings
+from src.comments.models import AbstractComment
+
+
+class Post(models.Model):
+    """Posts model"""
+    text = models.TextField(max_length=1000)
+    create_date = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=True)
+    moderation = models.BooleanField(default=True)
+    view_count = models.PositiveIntegerField(default=8)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Post by {self.user}'
+
+
+class Comment(AbstractComment, MPTTModel):
+    """Comments model for posts"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.post)
