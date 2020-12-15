f396b62591de1382b2b5695267f39968cc6f2f11
Kirill Andreev <Hizenberg228@,ail.ru>
2020-12-15 19:47:19 +0300

    Fixed bug with pysonet.settings

--- pysonet/pysonet/urls.py
+++ pysonet/pysonet/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from pysonet.pysonet import settings
+from pysonet import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
